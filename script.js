f9f4232e34cc6379bf8bac8a0848748a31dde632
quasarians <125194112+quasarians@users.noreply.github.com>
2023-02-28 11:37:17 +0530

    Roadmap Script

--- script.js
+++ script.js
@@ -50,19 +50,29 @@ function websiteVisits(response) {
 // Clock JS End
 
 
-// Sticky NavBar 
+var items = document.querySelectorAll(".timeline li");
 
-// const nav = document.querySelector('#navBar');
-// const header = document.querySelector('#head')
-// let navTop = header.offsetTop;
-// // console.log(navTop)
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
 
-// function fixedNav() {
-//         if (window.scrollY >= navTop) {
-//                 header.classList.add('stick');      
-//         } else {
-//                 header.classList.remove('stick');
-//         }
-// }
-
-// window.addEventListener('scroll', fixedNav);
+function callbackFunc() {
+  for (var i = 0; i < items.length; i++) {
+    if (isElementInViewport(items[i])) {
+      if(!items[i].classList.contains("in-view")){
+        items[i].classList.add("in-view");
+      }
+    } else if(items[i].classList.contains("in-view")) {
+        items[i].classList.remove("in-view");
+    }
+  }
+}
+ 
+window.addEventListener("load", callbackFunc);
+window.addEventListener("scroll", callbackFunc);

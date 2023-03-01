b0ec4ad6c3e24ae874d1ddadeda11d15aced643e
quasarians <125194112+quasarians@users.noreply.github.com>
2023-03-01 10:05:37 +0530

    Read more

--- script.js
+++ script.js
@@ -227,3 +227,19 @@ window.onload = function () {
     }, 1400);
   }, 1500);
 };
+
+function read_more() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}

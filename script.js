26e71b385801291ff438651530829ec8735bb980
quasarians <125194112+quasarians@users.noreply.github.com>
2023-03-13 13:26:12 +0530

    hackathon is live

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ var x = setInterval(function () {
         // If the count down is finished, write some text
         if (distance < 0) {
                 clearInterval(x);
-                document.getElementById("clock").innerHTML = "REGISTRATIONS CLOSED!";
+                document.getElementById("clock").innerHTML = "Hackathon is Live!🚀";
         }
 }, 1000);
 
